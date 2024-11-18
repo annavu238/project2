f59e6026846d0600127812ed2f6c979cf021623c
annavu238 <oanhktqd@gmail.com>
2024-11-18 13:38:40 -0800

    Update app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import matplotlib.pyplot as plt
+import altair as alt
+import seaborn as sns
+import plotly.express as px
+
+data = pd.read_csv('https://raw.githubusercontent.com/annavu238/project2/refs/heads/main/3510003801-eng.csv')
+st.table(data.head())
+
