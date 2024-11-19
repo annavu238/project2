5368f0b163081b4fde0c87adaa48204048681c3a
annavu238 <oanhktqd@gmail.com>
2024-11-18 22:48:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ import altair as alt
 import seaborn as sns
 import plotly.express as px
 
-data = pd.read_csv('https://raw.githubusercontent.com/annavu238/project2/refs/heads/main/3510003801-eng.csv', on_bad_lines='skip')
+data = pd.read_csv('https://raw.githubusercontent.com/annavu238/project2/refs/heads/main/social-media-impact-on-suicide-rates.csv', on_bad_lines='skip')
 st.table(data.head())
 
