886dcc3314c566684c459c489e64a48aa9cebffa
annavu238 <oanhktqd@gmail.com>
2024-11-18 23:41:54 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import altair as alt
 import seaborn as sns
 import plotly.express as px
 
-data = pd.read_csv('https://raw.githubusercontent.com/annavu238/project2/refs/heads/main/social-media-impact-on-suicide-rates.csv', on_bad_lines='skip')
+data = pd.read_csv('https://raw.githubusercontent.com/annavu238/project2/refs/heads/main/gym_members_exercise_tracking.csv', on_bad_lines='skip')
 st.table(data.head())
 # Analyze the percentage change in suicide rates since 2010 by year
 both_sexes = data[data['sex'] == 'BTSX']
